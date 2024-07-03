d89736fa276209f87a3aacfb60dc0f9f93a431cb
lonercode <penpalclassmate@gmail.com>
2024-07-03 17:49:21 +0100

    config for vercel

--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,5 @@
 pip3 install -r requirements.txt
 
 # make migrations
-python3.9 manage.py migrate 
-python3.9 manage.py collectstatic --noinput
+python3.11 manage.py migrate 
+python3.11 manage.py collectstatic --noinput
